79b8ea97a980224a5b5ca65db235ea5a40a7dcc1
Amir <amir.peleg140@gmail.com>
2022-08-21 10:51:11 +0300

    Added option to download videos

--- main.py
+++ main.py
@@ -1,60 +1,47 @@
-import sys
+import messages
 from pytube import YouTube
 import os
-import webbrowser
 from pytube.exceptions import VideoUnavailable, RegexMatchError
 from telegram.ext import *
 
-API_KEY = 'ENTER KEY HERE'
+API_KEY = '5559701057:AAFhU5ZWAtyoFRSeg-b4DuVIbBUlZDN2n1o'
 words_list = ['youtube', 'youtu', 'yt']
 
 
-def get_file_name(input_link):
-    yt = YouTube(input_link)
-    file_name = yt.title
-    return file_name
-
-
-def youtube_download(input_link, update, context):  # separate this function to download, video details and print
+def youtube_download(input_link, update, context):
     if any(word in input_link for word in words_list):
+        file_format = input_link[-3:]
+        input_link = input_link[:-3]
         yt = YouTube(input_link)
+        if file_format == 'vid':
+            video = yt.streams.get_highest_resolution()
+            file_format = '.mp4'
+            download_name = video.default_filename
+            check_dup(download_name, update, context)
+            out_file = video.download(output_path="")
+            messages.print_message(input_link, update, context, file_format)
 
-        audio = yt.streams.get_audio_only()
-        download_name = audio.default_filename.replace("mp4", "mp3")
+        else:
+            audio = yt.streams.get_audio_only()
+            file_format = '.mp3'
+            download_name = audio.default_filename
+            check_dup(download_name,update,context)
+            download_name = audio.default_filename.replace("mp4", "mp3")
+            out_file = audio.download(output_path="")
+            base, ext = os.path.splitext(out_file)
+            new_file = base + '.mp3'
 
-        if os.path.exists(download_name):
-            already_exist(update, context)
+            os.rename(out_file, new_file)
+            messages.print_message(input_link, update, context, file_format)
 
-        out_file = audio.download(output_path="")
-        base, ext = os.path.splitext(out_file)
-        new_file = base + '.mp3'
 
-        update.message.reply_text(f"Downloading now {get_file_name(input_link)}")
-        os.rename(out_file, new_file)
-        update.message.reply_text(f"Download {get_file_name(input_link)} finished!")
-        update.message.reply_text(yt.thumbnail_url)
-        update.message.reply_text("--------------------------------------------\n"
-                                  "--------------------------------------------\n"
-                                  "--------------------------------------------\n"
-                                  "--------------------------------------------\n"
-                                  "--------------------------------------------\n"
-                                  "Past any link to start a new download!\n"
-                                  "--------------------------------------------\n"
-                                  "--------------------------------------------\n"
-                                  "--------------------------------------------\n"
-                                  "--------------------------------------------\n"
-                                  "--------------------------------------------\n")
-        #webbrowser.open(get_file_name(input_link) + '.mp3')
-        update.message.reply_text(os.getcwd() + get_file_name(input_link) + '.mp3')
         # add telegram link to the path of the file
 
-
-def already_exist(update, context):
-    update.message.reply_text("File already exist!!\n"
-                              "Please try another link")
-    os.execl(sys.executable, sys.executable, *sys.argv)
-
-
+def check_dup(download_name, update,context):
+    if os.path.exists(download_name):
+        messages.already_exist(update, context)
+    if os.path.exists(download_name+'.mp4'):
+        messages.already_exist(update, context)
 def youtube_error(update, context):
     update.message.reply_text("Please use only youtube video links!")
 
@@ -70,6 +57,7 @@ def reply(update, context):
         youtube_error(update, context)
     # add exception handler file
     # add message and reply file
+
 
 def main():
     updater = Updater(API_KEY, use_context=True)
---
+++ messages.py
@@ -0,0 +1,32 @@
+import sys
+
+from pytube import YouTube
+import os
+
+def already_exist(update, context):
+    update.message.reply_text("File already exist!!\n"
+                              "Please try another link")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+def get_file_name(input_link):
+    yt = YouTube(input_link)
+    file_name = yt.title
+    return file_name
+
+def print_message (input_link, update, context, file_format):
+    yt = YouTube(input_link)
+    update.message.reply_text(f"Downloading now {get_file_name(input_link)}")
+    update.message.reply_text(f"Download {get_file_name(input_link)} finished!")
+    update.message.reply_text(yt.thumbnail_url)
+    update.message.reply_text(os.getcwd() + get_file_name(input_link) + file_format)
+    update.message.reply_text("--------------------------------------------\n"
+                              "--------------------------------------------\n"
+                              "--------------------------------------------\n"
+                              "--------------------------------------------\n"
+                              "--------------------------------------------\n"
+                              "Past any link to start a new download!\n"
+                              "--------------------------------------------\n"
+                              "--------------------------------------------\n"
+                              "--------------------------------------------\n"
+                              "--------------------------------------------\n"
+                              "--------------------------------------------\n")
